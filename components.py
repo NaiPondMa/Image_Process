cb3062047192637ee0d53ce2e678e258fa795cb7
NaiPondMa <118798814+NaiPondMa@users.noreply.github.com>
2026-08-17 14:21:56 +0700

    shrinking_function

--- components.py
+++ components.py
@@ -44,8 +44,43 @@ def manual_zoom_function(image, zoom_factor):
 
     return zoomed_image
 
-"""
-Testing by using matplotlib to display the original and zoomed images
+def manual_shrinking_function(image, shrink_factor):
+    """
+    ใช้ algorithm nearest neighbor ในการ shrink ภาพ
+    
+    Parameters:
+    image (numpy.ndarray): ภาพต้นฉบับที่ต้องการ shrink
+    shrink_factor (float): อัตราการ shrink ของภาพ (ค่ามากกว่า 1 คือ shrink in, ค่าน้อยกว่า 1 คือ shrink out)
 
-"""
+    returns:
+    numpy.ndarray: ภาพที่ถูก shrink แล้ว
+    """
+    # step 1: ตรวจสอบว่า shrink_factor เป็นค่าที่ถูกต้อง
+    if shrink_factor <= 0:
+        raise ValueError("shrink_factor must be greater than 0")
 
+    # step 2: convert ภาพเป็นขาวดํา
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # step 3: คํานวณขนาดของภาพใหม่
+    new_height = int(gray_image.shape[0] / shrink_factor)
+    new_width = int(gray_image.shape[1] / shrink_factor)
+
+    # step 4: สร้างภาพใหม่ที่มีขนาดตามที่คํานวณ
+    shrunk_image = np.zeros((new_height, new_width), dtype=image.dtype)
+
+    #step 5: ใช้ algorithm nearest neighbor ในการ shrink ภาพ
+    for i in range(new_height):
+        for j in range(new_width):
+            # คํานวณพิกัดของ pixel เริ้มต้นจากภาพที่ถูก shrink
+            orig_x = int(i * shrink_factor)
+            orig_y = int(j * shrink_factor)
+
+            # ตรวจสอบว่าพิกัดต้นฉบับอยู่ในขอบเขตของภาพต้นฉบับ กรณีมีก่ารปัดเศษส่วน floating
+            orig_x = min(orig_x, gray_image.shape[0] - 1)
+            orig_y = min(orig_y, gray_image.shape[1] - 1)
+
+            # นำ pixel จากภาพต้นฉบับมาใส่ในภาพที่ถูก shrink
+            shrunk_image[i, j] = gray_image[orig_x, orig_y]
+
+    return shrunk_image
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import cv2
 import matplotlib.pyplot as plt
 
-from function_zoom import manual_zoom_function
+from components import manual_zoom_function
+from components import manual_shrinking_function
 
 if __name__ == "__main__":
     image = cv2.imread(r"D:\Github\Image_Process\images\cartoon_6a7499925e31c.jpg")
@@ -10,13 +11,18 @@ if __name__ == "__main__":
         raise FileNotFoundError("Image not found: D:\Github\Image_Process\images\cartoon_6a7499925e31c.jpg")
 
     zoomed_image = manual_zoom_function(image, 2.0)
+    shrinked_image = manual_shrinking_function(image, 4.0)
 
-    plt.subplot(1, 2, 1)
+    plt.subplot(1, 3, 1)
     plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     plt.title("Original Image")
 
-    plt.subplot(1, 2, 2)
+    plt.subplot(1, 3, 2)
     plt.imshow(zoomed_image, cmap='gray')
     plt.title("Zoomed Image")
+
+    plt.subplot(1, 3, 3)
+    plt.imshow(shrinked_image, cmap='gray')
+    plt.title("Shrinked Image")
 
     plt.show()
